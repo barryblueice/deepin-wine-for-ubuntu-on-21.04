057375b0b06c5d4c73e3b907eaae462a1f769880
Barryblueice <44601454+barryblueice@users.noreply.github.com>
2021-10-30 16:21:04 +0800

    Add files via upload

---
+++ repair.sh
@@ -0,0 +1,7 @@
+sudo dpkg -i  python3-dbus_1.2.16-5_amd64.deb
+sudo dpkg -i python-gobject_3.34.0+2.28.6-14ubuntu1_all.deb
+sudo dpkg -i python-gi_3.36.0-1_amd64.deb
+sudo dpkg -i python-gobject-2_2.28.6-14ubuntu1_amd64.deb
+sudo dpkg -i python-gobject_3.34.0+2.28.6-14ubuntu1_all.deb
+sudo dpkg -i python-dbus_1.2.16-1build1_amd64.deb
+
